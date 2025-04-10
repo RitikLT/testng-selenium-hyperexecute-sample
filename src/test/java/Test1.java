253de19fc9785bb74cab0a85d9e7a114af7afa43
Ritik Gagde <ritikg@Mac.lan>
2025-04-10 21:15:01 +0530

    added

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -34,7 +34,7 @@ public class Test1
         capabilities.setCapability("platform", System.getenv("HYPEREXECUTE_PLATFORM"));
         capabilities.setCapability("browserName", browser);
         capabilities.setCapability("version", version);
-
+        capabilities.setCapability("selenium_version", "4.0.0");
         capabilities.setCapability("tunnel",false);
         capabilities.setCapability("network",true);
         capabilities.setCapability("console",true);
